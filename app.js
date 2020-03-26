3bd6370aaf0c9e39991b6ec3d075956f55be8036
Mart <mcpkmart@hotmail.nl>
2020-03-26 14:40:02 +0100

    Login and register

--- app.js
+++ app.js
@@ -3,24 +3,41 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash = require('connect-flash');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var morgan = require('morgan');
 
 var app = express();
 
+// Database connection
+var configDB = require('./config/database.js');
+mongoose.connect(configDB.url);
+
+require('./config/passport')(passport);
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
+app.use(morgan('dev'));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(bodyParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Passport Setup
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' }))
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+// load our routes and pass in our app and fully configured passport
+require('./routes/routes.js')(app, passport); 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -32,6 +49,9 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  console.log(err.message);
+  console.log(err.status);
 
   // render the error page
   res.status(err.status || 500);
